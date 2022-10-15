bbdf01e7084f098f6713213d4026d0b9dbdf0404
tiep-em <tiepkhodo@gmail.com>
2022-10-15 09:16:51 +0700

    upload get Factorial

--- src/com/tiepk/mathutil/core/MathUtil.java
+++ src/com/tiepk/mathutil/core/MathUtil.java
@@ -23,27 +23,58 @@ public class MathUtil {
     // Vì giai thừa tăng cựa nhanh, nên 21! đã vượt quá 18 số 0
     // tràn kiểu long
     // Ta ko tính 21! trở lên
+    
+    //Sửa HÀM TÍNH GIAI THỪA = CÁCH XÀI 
+    //n! = 1.2.3.4.5....n
+    //5!= 1.2.3.4.5
+    //5! = 4! x 5 = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1!
+    //1! = 1 chốt đon
+    //n! = n x (n-1!) công thức đệ quy
+    //N! = RETURN 
+    //ĐỆ QUY: GỌI LẠI CHÍNH MÌNH VỚI 1 QUY MÔ KHAC/NHỎ HƠN
+    //CON BÚP BÊ NGA
     public static long getFactorial(int n) {
 
         if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
         
-
         if (n == 0 || n == 1) 
             return 1;
         
-
-        //sống sót đến đây, sure n = 2...20!!!
-        //CẤM KO SÀI ELSE NỮA KHI HÀM ĐÃ CÓ RETURN PHÍA TRƯỚC
-        long product = 1;// biến cộng dồn, nhân dồn, biến con heo đất
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        
-
-        return product;
+        return n * getFactorial(n-1);
+       
 
     }
+    
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20");
+//        
+//       
+//
+//        if (n == 0 || n == 1) 
+//            return 1;
+//        
+//
+//        //sống sót đến đây, sure n = 2...20!!!
+//        //CẤM KO SÀI ELSE NỮA KHI HÀM ĐÃ CÓ RETURN PHÍA TRƯỚC
+//        long product = 1;// biến cộng dồn, nhân dồn, biến con heo đất
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        
+//
+//        return product;
+//
+//    }
+    
 }
+
+
+
 
 //CODING CONVERTION: QUY TẮC VIẾT CODE CTY ÉP PHẢI THEO!!!!
 //ALT-SHIFT-F CĂN CODE TỰ ĐỘNG, KO LẠM DỤNG
